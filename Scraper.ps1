e3b1410e05dcd45b09f8b4d801cab92030c5f931
Richard Ogin <rogin@users.noreply.github.com>
2023-01-09 23:34:14 -0600

    Refactors
    -add Requires to top of script
    -corrected examples for plugin names
    -move 1Pass UUID into editable section
    -quiet the iwr
    -improve some function names
    -add more debugging
    -add switch to include attachment downloads
    -download plugins before attachments
    -plugin download issues no longer abort
    -warn not error when support level prevents download

--- Scraper.ps1
+++ Scraper.ps1
@@ -1,6 +1,12 @@
-﻿# list of plugins to download
-# plugin list as of 6 Jan 2023
+﻿#Requires -Version 6
+#Requires -Modules powerhtml
+
+Import-Module powerhtml
+
 <#
+    # List of plugins to download
+    # Last updated: 6 Jan 2023
+    
     Advanced Alerting                               Gold or above
     Advanced Clustering                             Gold or above
     ASTM E1381 Transmission Mode                    Gold or above
@@ -18,35 +24,33 @@
     Multi-Factor Authentication                     Platinum Only
     Role-Based Access Control (User Authorization)  Gold or above
     Serial Connector                                Gold or above
-    SSL Manager
+    SSL Manager                                     Core Extension Bundle or above
 #>
-# we can request all with an empty list
+# we can request all plugins by providing an empty list
 $PluginNames = @()
-#$PluginNames = @('LDAP Authentication','Role-Based Access Control Plug-In', 'SSL Manager Plug-In', 'Multi-Factor Authentication')
-# version to download
+#$PluginNames = @('LDAP Authentication','Role-Based Access Control (User Authorization)', 'SSL Manager', 'Multi-Factor Authentication', 'FHIR Connector (R4)')
+
+# plugin version to download
 $PluginVersion = "4.2"
+
+#hard-coded UUID within 1Password account
+$1PASS_UUID = 'j5m7piroikq3dznzojyjmodyja'
 
 $BaseUrl = "https://www.community.nextgen.com"
 $LoginUrl = $BaseUrl + "/apex/SuccessCommunityLogin"
 $PluginListUrl = $BaseUrl + "/optimization/articles/Hot_Topic/Mirth-Plug-In-Central"
 
-# quiet this chatty portion
-$PSDefaultParameterValues['Invoke-WebRequest:Verbose'] = 'False'
-
-Import-Module powerhtml
-
 # this will hold our cookies and be used in (most) web requests
 $session = [Microsoft.PowerShell.Commands.WebRequestSession]::new()
 
-#currently obtains creds via rogin's 1password-cli integration
+#currently obtains creds via 1password-cli integration
 function ObtainCredentials {
     [CmdletBinding()]
     param ()
 
     Invoke-Expression $(op signin)
     
-    #hard-coded UUID from rogin's account
-    $json = op get item 'j5m7piroikq3dznzojyjmodyja' --fields username,password | ConvertFrom-Json
+    $json = op get item $1PASS_UUID --fields username,password | ConvertFrom-Json
     
     op signout
 
@@ -54,13 +58,13 @@ function ObtainCredentials {
 }
 
 # verified my work with his - https://xceptionale.wordpress.com/2016/04/15/reverse-engineering-the-salesforce-site-login-process/
-function SiteLogin {
+function Invoke-Login {
     [CmdletBinding()]
     param ()
 
-    Write-Debug "Logging into site"
-    Write-Verbose "Connecting to $LoginUrl"
+    Write-Verbose "Logging into site"
 
+    Write-Debug "Connecting to $LoginUrl"
     $login = Invoke-WebRequest -Uri $LoginUrl -WebSession $session
 
     ## THIS IS FOR PS 5 ##
@@ -99,28 +103,27 @@ function SiteLogin {
     # extract the form's Action? worked fine so far without
     #$action = 'https://www.community.nextgen.com/SuccessCommunityLogin?refURL=http%3A%2F%2Fwww.community.nextgen.com%2Fapex%2FSuccessCommunityLogin'
     $action = $LoginUrl
-    Write-Verbose "Invoking action $action with login credentials"
+    Write-Debug "POSTing login credentials to $action"
     # post the login request
     $loginResponse = Invoke-WebRequest -Uri $action -Method Post -Body $content -WebSession $session
 
-    Write-Verbose "Parsing javascript"
+    Write-Debug "Parsing javascript"
     #parse window.location.href from javascript in $loginResponse.Content
     #ex. https://www.community.nextgen.com/secur/frontdoor.jsp?allp=1&apv=1&cshc=y000005LbAH00000007MP6&refURL=https%3A%2F%2Fwww.community.nextgen.com%2Fsecur%2Ffrontdoor.jsp&retURL=%2Fapex%2FMainCommunityLanding&sid=00D400000007MP6%21ARYAQFEjuQtfFH4xlA5bhl7aZ0QQsqgvqHx8JQse8kagSX4XhOXRTRxMUdJ2qwdAfvmDKeRRnBsKuYpp8MlGaxmIQTDt11UB&untethered=
     $extractedHref = parseJavascript $loginResponse.Content ";"
 
-    Write-Verbose "Invoking $extractedHref"
+    Write-Debug "Invoking $extractedHref"
     # call the href that was in the javascript
     $webResponse = Invoke-WebRequest -Uri $extractedHref -WebSession $session
 
-    Write-Verbose "Parsing javascript"
+    Write-Debug "Parsing javascript"
     #again, parse window.location.href from javascript in $webResponse.Content
     #ex. '/apex/MainCommunityLanding'
     $extractedHref = $BaseUrl + (parseJavascript $webResponse.Content "}")
 
-    Write-Verbose "Invoking $extractedHref"
+    Write-Debug "Invoking $extractedHref"
     # call the href that was in the javascript
-    $finalResponse = Invoke-WebRequest -Uri $extractedHref -WebSession $session
-    #Write-Verbose "Final response: $finalResponse"
+    Invoke-WebRequest -Uri $extractedHref -WebSession $session | Out-Null
 }
 
 #for a given wall of text, parse the value of 'window.location.href'
@@ -141,13 +144,13 @@ function parseJavascript($text, $lineDelimiter) {
     $subtext.Substring($urlIndex, $endTickIndex - $urlIndex)
 }
 
-function GetPluginLinks {
+function Select-PluginLinks {
     [CmdletBinding()]
     param ()
 
-    Write-Debug "Getting plugin links"
-    Write-Verbose "Invoking $PluginListUrl"
+    Write-Verbose "Selecting plugin links"
 
+    Write-Debug "Invoking $PluginListUrl"
     $WebContent = Invoke-WebRequest -Uri $PluginListUrl -WebSession $session | ConvertFrom-Html
 
     # find the links where the href contains 'articles' as the others link to the Support Levels
@@ -159,116 +162,144 @@ function GetPluginLinks {
     #user can provide an empty list indicating all plugins
     if($PluginNames.Count -eq 0) {
         Write-Debug "Returning all plugins"
-        return $links
+        $links
     } else {
-        return $links | Where-Object -FilterScript {$PluginNames -contains $_.InnerText.Trim()}
+        @($links | Where-Object -FilterScript {$PluginNames -contains $_.InnerText.Trim()})
     }
 }
 
-function GetPluginPage {
+function Read-PluginPage {
     [CmdletBinding()]
     param (
-        # Parameter help description
+        # Node containing plugin download link
         [Parameter(Mandatory,Position=0,ValueFromPipeline=$true)]
         [HtmlAgilityPack.HtmlNode]
-        $Node
+        $Node,
+        # switch to include attachments along with the plugin
+        [switch]
+        $IncludeAttachments
     )
 
     Process {
-        Write-Host ("Processing '{0}'" -f $Node.InnerText)
+        Write-Verbose "Processing '$($Node.InnerText)', `$IncludeAttachments=$IncludeAttachments"
 
         $href = $BaseUrl + $Node.GetAttributeValue('href', 'missing-href-value-1')
         
         if($href -eq 'missing-href-value-1') {
-            Write-Debug "Could not find href value for $($Node.InnerText) - you likely don't have the correct support level, skipping"
-            return
-        }
+            Write-Warning "Could not find href value for $($Node.InnerText) - you likely don't have the correct support level, skipping"
+        } else {
+            Write-Debug "Invoking plugin `$href=$href"
+            $WebContent = Invoke-WebRequest -Uri $href -WebSession $session | ConvertFrom-Html
 
-        $WebContent = Invoke-WebRequest -Uri $href -WebSession $session | ConvertFrom-Html
+            $pluginDownloadLinks = $WebContent.SelectNodes("(//div[@class='pbSubsection'])[6]//table[@class='htmlDetailElementTable']//td//a")
 
-        # download all attachments
-        $attachments = $WebContent.SelectNodes("(//div[@class='pbSubsection'])[7]//table[@class='detailList']//a")
+            #example text: "Advanced Alerting  Plug-in 4.2"
+            $pluginDownloadLinks = @($pluginDownloadLinks | Where-Object {$_.InnerText.Trim().EndsWith($PluginVersion)})
 
-        foreach ($attachment in $attachments) {
-            $filename = $attachment.InnerText.Trim()
-
-            if($filename -eq '') {
-                Write-Debug "Attachment filename was empty, skipping"
+            Write-Verbose "Found $($pluginDownloadLinks.Count) links matching version '$PluginVersion'"
+            
+            if($pluginDownloadLinks.Count -eq 0) {
+                Write-Warning "Failed to find version '$PluginVersion' of $($Node.InnerText)"
+            } elseif($pluginDownloadLinks.Count -gt 1) {
+                Write-Error "Found $($pluginDownloadLinks.Count) download links for version '$PluginVersion' of $($Node.InnerText), expected 1"
             } else {
-                if(-not $filename.EndsWith(".pdf")) {
-                    Write-Verbose "Appending .pdf to filename"
-                    $filename += ".pdf"
-                }
+                $NameAndVersion = $pluginDownloadLinks[0].InnerText.Trim()
 
-                Write-Host "Downloading attachment '$filename'"
-                $href = $BaseUrl + $attachment.GetAttributeValue('href', 'missing-attachment-href-value')
+                #ex. https://www.community.nextgen.com/apex/ResourceRepository?fileId=a9o4y000000YIdT
+                $downloadUrl = $pluginDownloadLinks[0].GetAttributeValue('href', 'missing-href-value-2')
+
+                Write-Debug "Beginning download of plugin '$NameAndVersion' from $downloadUrl"
+
+                $downloadResponse = Invoke-WebRequest -Uri $downloadUrl -WebSession $session
+                #parse window.location.href from javascript in $downloadResponse.Content
+                #ex. /DownloadSuccess?fileId=a9o4y000000YId9
+                Write-Debug "Parsing javascript"
+                $extractedHref = $BaseUrl + (parseJavascript $downloadResponse.Content ";")
+
+                Write-Debug "Invoking $extractedHref"
+
+                # call the href that was in the javascript
+                $secondResponse = Invoke-WebRequest -Uri $extractedHref -WebSession $session | ConvertFrom-Html
                 
-                Invoke-WebRequest -Uri $href -WebSession $session -OutFile $filename
+                #if the plugin is at Support Level "Platinum Only", of which I don't have,
+                #then the link will send us to a forbidden page that will fail parsing for the hidden "a" tag below.
+                $hiddenLink = $secondResponse.SelectSingleNode("//a[@class='hidden']")
+
+                if($null -eq $hiddenLink) {
+                    Write-Warning "Failed to find download link - you likely don't have the correct support level, skipping"
+                } else {
+                    # extract final HREF
+                    # ex. https://nextgen-aws-salesforce-prod-sdrive-us-east-2.s3.us-east-2.amazonaws.com/a9o4y000000YId9AAG/ldap-3.12.0.b1752.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIA2ZO6ZFSFWXJS7NWF%2F20220318%2Fus-east-2%2Fs3%2Faws4_request&amp;X-Amz-Date=20220318T051704Z&amp;X-Amz-Expires=216000&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=962a48d81f6091b86855eb2041df9bd0ba3123479ac87cc860bc061aa82edaff
+                    $extractedHref = $hiddenLink.GetAttributeValue('href', 'missing-href-value-3')
+                    Write-Debug "Found `$hiddenLink with `$extractedHref=$extractedHref"
+
+                    Write-Debug "Decoding `$extractedHref"
+                    # decode this or else the AWS call will fail
+                    [uri]$decodedHref = [System.Web.HttpUtility]::HtmlDecode($extractedHref)
+                    # extract filename
+                    $filename = $decodedHref.Segments[-1]
+
+                    Write-Debug "Downloading plugin '$filename' from $($decodedHref.AbsoluteUri)"
+
+                    Invoke-WebRequest -Uri $decodedHref.AbsoluteUri -OutFile $filename
+                }
             }
         }
 
-        $pluginDownloadLinks = $WebContent.SelectNodes("(//div[@class='pbSubsection'])[6]//table[@class='htmlDetailElementTable']//td//a")
+        if($IncludeAttachments) {
+            # download all attachments
+            $attachments = $WebContent.SelectNodes("(//div[@class='pbSubsection'])[7]//table[@class='detailList']//a")
 
-        #example text: "Advanced Alerting  Plug-in 4.2"
-        $pluginDownloadLinks = @($pluginDownloadLinks | where {$_.InnerText.Trim().EndsWith($PluginVersion)})
+            foreach ($attachment in $attachments) {
+                $filename = $attachment.InnerText.Trim()
 
-        Write-Debug "Found $($pluginDownloadLinks.Count) links matching version '$PluginVersion'"
-        
-        if($pluginDownloadLinks.Count -eq 0) {
-            Write-Error "Failed to find version '$PluginVersion' of $($Node.InnerText)"
-            return
-        } elseif($pluginDownloadLinks.Count -gt 1) {
-            Write-Error "Found $($pluginDownloadLinks.Count) download links for version '$PluginVersion' of $($Node.InnerText)"
-            return
+                if($filename -eq '') {
+                    Write-Warning "Attachment filename is empty, skipping"
+                } else {
+                    if(-not $filename.EndsWith(".pdf")) {
+                        Write-Verbose "Appending .pdf to filename"
+                        $filename += ".pdf"
+                    }
+
+                    Write-Verbose "Downloading attachment '$filename'"
+                    $href = $BaseUrl + $attachment.GetAttributeValue('href', 'missing-attachment-href-value')
+
+                    Write-Debug "`$filename=$filename, `$href=$href"
+
+                    Invoke-WebRequest -Uri $href -WebSession $session -OutFile $filename
+                }
+            }
         }
-
-        $NameAndVersion = $pluginDownloadLinks[0].InnerText.Trim()
-
-        Write-Host "Downloading plugin '$NameAndVersion'"
-
-        #ex. https://www.community.nextgen.com/apex/ResourceRepository?fileId=a9o4y000000YIdT
-        $downloadUrl = $pluginDownloadLinks[0].GetAttributeValue('href', 'missing-href-value-2')
-
-        $downloadResponse = Invoke-WebRequest -Uri $downloadUrl -WebSession $session
-        #parse window.location.href from javascript in $downloadResponse.Content
-        #ex. /DownloadSuccess?fileId=a9o4y000000YId9
-        $extractedHref = $BaseUrl + (parseJavascript $downloadResponse.Content ";")
-
-        # call the href that was in the javascript
-        $secondResponse = Invoke-WebRequest -Uri $extractedHref -WebSession $session | ConvertFrom-Html
-		
-        #if the plugin is at Support Level "Platinum Only" which we don't have, then the link will send us to a forbidden page that will fail parsing for the hidden "a" tag below
-        $hiddenLink = $secondResponse.SelectSingleNode("//a[@class='hidden']")
-
-        if($null -eq $hiddenLink) {
-            Write-Debug "Failed to find download link - you likely don't have the correct support level, skipping"
-            return
-        }
-
-        # extract final HREF
-        # ex. https://nextgen-aws-salesforce-prod-sdrive-us-east-2.s3.us-east-2.amazonaws.com/a9o4y000000YId9AAG/ldap-3.12.0.b1752.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIA2ZO6ZFSFWXJS7NWF%2F20220318%2Fus-east-2%2Fs3%2Faws4_request&amp;X-Amz-Date=20220318T051704Z&amp;X-Amz-Expires=216000&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=962a48d81f6091b86855eb2041df9bd0ba3123479ac87cc860bc061aa82edaff
-        $extractedHref = $hiddenLink.GetAttributeValue('href', 'missing-href-value-3')
-        # decode this or else the AWS call will fail
-        [uri]$decodedHref = [System.Web.HttpUtility]::HtmlDecode($extractedHref)
-        # extract filename
-        $filename = $decodedHref.Segments[-1]
-
-        # call the href that was in the javascript
-        Invoke-WebRequest -Uri $decodedHref.AbsoluteUri -OutFile $filename
     }
 }
 
-function Scrape {
+function Start-Scrape {
     [CmdletBinding()]
     param ()
 
-    $ErrorActionPreference = 'Stop'
+    begin {
+        $StoredPSDefaultParameterValues = $PSDefaultParameterValues.Clone()
+        $StoredProgressPreference = $ProgressPreference
+        
+        # quiet this chatty portion
+        $PSDefaultParameterValues.Add('Invoke-WebRequest:Debug', $False)
+        $PSDefaultParameterValues.Add('Invoke-WebRequest:Verbose', $False)
 
-    SiteLogin
+        $ProgressPreference = 'SilentlyContinue'
+    }
 
-    $pluginLinks = GetPluginLinks
-    
-    $pluginLinks | GetPluginPage
+    process {
+        Invoke-Login
+
+        $pluginLinks = Select-PluginLinks
+        
+        $pluginLinks | Read-PluginPage -IncludeAttachments
+    }
+
+    end {
+        $PSDefaultParameterValues = $StoredPSDefaultParameterValues
+        $ProgressPreference = $StoredProgressPreference
+    }
 }
 
-Scrape -Debug -Verbose
+Start-Scrape -Debug -Verbose
